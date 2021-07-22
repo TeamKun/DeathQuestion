60004dcba9ebcb1619d1efec3d11f6669119b81d
dai-1103 <bosatsunumnum@gmail.com>
2021-07-23 01:17:54 +0900

    create project

---
+++ src/main/java/net/kunmc/lab/deathquestion/DeathQuestion.java
@@ -0,0 +1,17 @@
+package net.kunmc.lab.deathquestion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathQuestion extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
