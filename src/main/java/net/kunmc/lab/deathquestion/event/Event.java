3c86fe4460e3b5d816a27fd5f3a193633e673384
dai-1103 <bosatsunumnum@gmail.com>
2021-07-28 04:37:59 +0900

    ペナルティ処理を実装

--- src/main/java/net/kunmc/lab/deathquestion/event/Event.java
+++ src/main/java/net/kunmc/lab/deathquestion/event/Event.java
@@ -1,14 +1,18 @@
 package net.kunmc.lab.deathquestion.event;
 
+import net.kunmc.lab.deathquestion.config.Config;
+import net.kunmc.lab.deathquestion.config.ExecutionMethod;
 import net.kunmc.lab.deathquestion.game.Manager;
 import net.kunmc.lab.deathquestion.userInterface.ActionBarLogic;
 import net.kunmc.lab.deathquestion.userInterface.BossBarLogic;
 import net.kunmc.lab.deathquestion.userInterface.NameTagLogic;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Event implements Listener {
     @EventHandler(ignoreCancelled = true)
@@ -31,5 +35,23 @@ public class Event implements Listener {
         Player player = event.getPlayer();
         // バーを表示
         BossBarLogic.addPLayer(player);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        // 開票中にログアウトしたときのペナルティ
+        if (!Manager.isOpening()) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+
+        // アイテムをクリア
+        player.getInventory().clear();
+
+        // 処刑方法がスペクテイターに変更の場合
+        if (Config.executionMethod().equals(ExecutionMethod.CHANGE_TO_SPECTATOR)) {
+            player.setGameMode(GameMode.SPECTATOR);
+        }
     }
 }
